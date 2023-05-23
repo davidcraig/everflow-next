65744d38a1da348ae08b65de53dd64e5ca568791
David Craig <david@dcraig.dev>
2023-05-23 17:05:05 +0100

    Improve cards

--- src/WeatherCard.tsx
+++ src/WeatherCard.tsx
@@ -1,28 +1,14 @@
 import type WeatherCardData from "@/types/WeatherCardData"
 
 const WeatherCard = ({ date, desc, temp, icon }: WeatherCardData) => {
+  const emoji = temp >= 30 ? "🥵" : temp > 14 ? "🙂" : "🥶"
+
   return (
     <div className="card">
       <p className="date">{date}</p>
-      <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} />
+      <img width={100} height={100} src={`https://openweathermap.org/img/wn/${icon}@2x.png`} />
       <p className="description">{desc}</p>
-      <p className="temp">{temp}°c
-        {
-          temp >= 30 && (
-            <span>🥵</span>
-          )
-        }
-        {
-          temp < 30 && temp > 14 && (
-            <span>🙂</span>
-          )
-        }
-        {
-          temp <= 14 && (
-            <span>🥶</span>
-          )
-        }
-      </p>
+      <p>{temp}°c {emoji}</p>
     </div>
   )
 }
